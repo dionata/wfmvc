66d985d7a7edee31088c837761d7e87b02531fa9
jmoisespg@gmail.com <jmoisespg@gmail.com@37f5908c-2300-31e3-27a9-35a9dca1bc7c>
2011-10-21 16:52:36 +0000

    Envio inicial. A conversão direta de formulário para entidade e virse-versa já é possível. O Validador já contém os eventos de validação.

---
+++ trunk/Windows/Forms/Attributes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WFMVC.Windows.Forms
+{
+    public class ViewAttribute : Attribute
+    {
+        public Type Type { get; private set; }
+
+        public ViewAttribute(Type type)
+        {
+            this.Type = type;
+        }
+    }
+}
