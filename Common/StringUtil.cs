e3f022d7056717c39e097482b02c2d40e10a8869
jmoisespg <jmoisespg@gmail.com>
2011-10-21 16:52:36 +0000

    Envio inicial. A conversão direta de formulário para entidade e virse-versa já é possível. O Validador já contém os eventos de validação.

---
+++ Common/StringUtil.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NHibernate;
+
+namespace WFMVC.Common
+{
+    public static class StringUtil
+    {
+        /// <summary>
+        /// Gera o SQL de uma ICriteria
+        /// </summary>
+        /// <param name="criteria">a criteria a ser logada</param>
+        /// <returns></returns>
+        public static string GenerateSQL(this ICriteria criteria)
+        {
+            NHibernate.Impl.CriteriaImpl criteriaImpl = (NHibernate.Impl.CriteriaImpl)criteria;
+            NHibernate.Engine.ISessionImplementor session = criteriaImpl.Session;
+            NHibernate.Engine.ISessionFactoryImplementor factory = session.Factory;
+
+            NHibernate.Loader.Criteria.CriteriaQueryTranslator translator =
+                new NHibernate.Loader.Criteria.CriteriaQueryTranslator(
+                    factory,
+                    criteriaImpl,
+                    criteriaImpl.EntityOrClassName,
+                    NHibernate.Loader.Criteria.CriteriaQueryTranslator.RootSqlAlias);
+
+            String[] implementors = factory.GetImplementors(criteriaImpl.EntityOrClassName);
+
+            NHibernate.Loader.Criteria.CriteriaJoinWalker walker = new NHibernate.Loader.Criteria.CriteriaJoinWalker(
+                (NHibernate.Persister.Entity.IOuterJoinLoadable)factory.GetEntityPersister(implementors[0]),
+                                    translator,
+                                    factory,
+                                    criteriaImpl,
+                                    criteriaImpl.EntityOrClassName,
+                                    session.EnabledFilters);
+
+            return walker.SqlString.ToString()
+                .Replace(",", "," + Environment.NewLine + "\t")
+                .Replace("left", Environment.NewLine + "left")
+                .Replace("FROM", Environment.NewLine + "FROM")
+                .Replace("WHERE", Environment.NewLine + "WHERE")
+                .Replace("ORDER BY", Environment.NewLine + "ORDER BY");
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,22 @@
+﻿***********************************************************************************************
+WFMVC é uma biblioteca de utilitários para aplicações Windows Forms com a implementação 
+do padrão MVC.
+Possui Logger (Log4Net), Validação, Extensões MVC para Windows Forms.
+
+Para utilizar a biblioteca, basta adicioná-la às referências do seu projeto.
+
+1. Logger: O simples logger somente precisa ser invocado no momento em que for necessário 
+o log de algum evento na aplicação. Aconselhamos a utilização do log como Propriedade 
+privada ou protegida da sua classe, para que seus recursos sejam utilizados corretamente.
+
+2. Exceptions: Ainda não finalizado.
+
+3. Validação: O sistema de validação se baseia em um Enum e uma classe Validator.
+Instancie um Validator no cabeçalho da sua classe para que ele esteja disponível.
+Adicione um método para os eventos delegados do validator caso queira utilizá-lo dinamicamente
+em campos de texto de informação.
+
+4. O provedor MvcFormApplication fornece métodos de extensão para objetos e subtipos de 
+System.Windows.Forms, para que seu conteúdo seja traduzido em Modelos de Domínio (Entidades).
+Para tanto basta que seu formulário contenha os controles com os nomes idênticos 
+às propriedades da sua classe de Modelo.
