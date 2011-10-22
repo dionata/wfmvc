1d725a96755290f03b42bd6420caffba684a079e
jmoisespg <jmoisespg@gmail.com>
2011-10-22 14:34:37 +0000

    Inclusão de projeto de exemplo.

---
+++ SampleFormApplication/Model/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleFormApplication.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Cidade { get; set; }
+    }
+}
